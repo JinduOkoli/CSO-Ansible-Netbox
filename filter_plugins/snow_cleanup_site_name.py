d4d3d3cf8ae210adffe535e6f959f1323833466f
Calvin Remsburg <packetferret@gmail.com>
2020-06-05 05:16:49 -0500

    cleanup site name from snow

---
+++ filter_plugins/snow_cleanup_site_name.py
@@ -0,0 +1,12 @@
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'find_cpe': self.snow_cleanup_site_name,
+        }
+
+    def snow_cleanup_site_name(self, value):
+        # cpe = {}
+        site_name = value.replace(" ", "-")
+        site_name = site_name.lower()
+        return site_name        
