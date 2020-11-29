7caae257e4a256edabe794ac40b4037efe95198a
JinduOkoli <46223684+JinduOkoli@users.noreply.github.com>
2020-11-28 20:49:10 -0500

    Update cleanup_site_data.py

--- filter_plugins/cleanup_site_data.py
+++ filter_plugins/cleanup_site_data.py
@@ -11,9 +11,9 @@ class FilterModule(object):
         for each in value:
             site["name"] = each["name"]
             site["slug"] = each["slug"]
-            #site["region"] = each["region"]["slug"]
+            # site["region"] = each["region"]["slug"]
             site["country"] = each["region"]["name"]
-            #site["tenant"] = each["tenant"]["slug"]
+            # site["tenant"] = each["tenant"]["slug"]
             site["facility"] = each["facility"]
             site["asn"] = each["asn"]
             site["time_zone"] = each["time_zone"]
@@ -23,8 +23,8 @@ class FilterModule(object):
             site["street_address"] = address[0]
             site["city"] = str(address[1]).strip()
             site["state"] = str(address[2]).strip()
-            #site["latitude"] = each["latitude"]
-           # site["longitude"] = each["longitude"]
+            # site["latitude"] = each["latitude"]
+            # site["longitude"] = each["longitude"]
             site["contact_name"] = each["contact_name"]
             site["contact_phone"] = each["contact_phone"]
             site["contact_email"] = each["contact_email"]
