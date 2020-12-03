dc310e98a58114575e08c8014ab057bf50e382e2
JinduOkoli <46223684+JinduOkoli@users.noreply.github.com>
2020-12-03 12:50:35 -0500

    Update cleanup_claim_codes.py

--- filter_plugins/cleanup_claim_codes.py
+++ filter_plugins/cleanup_claim_codes.py
@@ -5,5 +5,5 @@ class FilterModule(object):
             'cleanup_claim_codes': self.cleanup_claim_codes,
         }
 
-    def cleanup_claim_code(self, value):
+    def cleanup_claim_codes(self, value):
         return (value.strip(",").replace(" ", "").split(","))
